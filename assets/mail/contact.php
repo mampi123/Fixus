0ed61108dfeb8c59721b8ea369ee263403f519ec
Martina Mampel <mampelmartina@gmail.com>
2025-02-15 11:08:36 -0300

    listo mail

--- assets/mail/contact.php
+++ assets/mail/contact.php
@@ -1,8 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 if (!$_POST) exit;
 
 // Función para validar la dirección de correo (no modificar)
